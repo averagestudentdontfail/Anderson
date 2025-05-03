9e976015604df522ba611f38e3ddabeb0c292211
Kiran <send2kiran.n@gmail.com>
2025-05-03 23:13:27 +1000

    current.

--- build/engine/alo/opt/vector.d
+++
@@ -1,6 +0,0 @@
-build/engine/alo/opt/vector.o: src/engine/alo/opt/vector.cpp \
- src/engine/alo/opt/vector.h sleef/include/sleef.h \
- src/engine/alo/opt/simd.h
-src/engine/alo/opt/vector.h:
-sleef/include/sleef.h:
-src/engine/alo/opt/simd.h:
