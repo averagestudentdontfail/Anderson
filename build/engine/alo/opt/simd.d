2f35d820276f431b989ef2eb069f1773b78d2259
Kiran <send2kiran.n@gmail.com>
2025-05-05 22:38:32 +1000

    yes

---
+++ build/engine/alo/opt/simd.d
@@ -0,0 +1,6 @@
+build/engine/alo/opt/simd.o: src/engine/alo/opt/simd.cpp \
+ src/engine/alo/opt/simd.h src/engine/alo/opt/../num/float.h \
+ sleef/include/sleef.h
+src/engine/alo/opt/simd.h:
+src/engine/alo/opt/../num/float.h:
+sleef/include/sleef.h:
--- build/engine/alo/opt/vector.d
+++
@@ -1,7 +0,0 @@
-build/engine/alo/opt/vector.o: src/engine/alo/opt/vector.cpp \
- src/engine/alo/opt/vector.h src/engine/alo/opt/../num/float.h \
- src/engine/alo/opt/simd.h sleef/include/sleef.h
-src/engine/alo/opt/vector.h:
-src/engine/alo/opt/../num/float.h:
-src/engine/alo/opt/simd.h:
-sleef/include/sleef.h:
